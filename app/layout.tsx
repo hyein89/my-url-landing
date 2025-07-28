bdeb1d4669f21f3f7f7dd4ba1151b672c7e0327a
hyein89 <bihontai@gmail.com>
2025-07-29 02:14:15 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "QuickShort - Shorten URL",
+  description: "Layanan untuk memperpendek tautan secara cepat dan gratis",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
