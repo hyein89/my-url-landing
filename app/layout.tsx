3b8e8fc9fb6b79053043f64eb63c902f822c9649
hyein89 <bihontai@gmail.com>
2025-07-29 02:15:31 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+// Jika kamu letakkan di app/globals.css:
+import "./globals.css";
+
 export const metadata = {
   title: "QuickShort - Shorten URL",
   description: "Layanan untuk memperpendek tautan secara cepat dan gratis",
